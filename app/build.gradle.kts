e33ea59222bdcb8fd448586f10c1302a3f859b35
desarollofasemovil <desarrollofasemovil@hotmial.com>
2025-09-27 10:26:25 -0500

    correcciones login y webviewSection.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.tramites1cero1.tramiappquibdo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Centralizacion"
+rootProject.name = "tramiappquibdo"
 include(":app")
  
